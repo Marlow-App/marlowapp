8354c95b1caa348d18a828c1911e614c83b25305
Marlow-App <bymarloww@gmail.com>
2026-04-15 12:16:15 +0900

    Update schema.ts

--- shared/schema.ts
+++ shared/schema.ts
@@ -12,7 +12,7 @@ import {
 } from "drizzle-orm/pg-core";
 
 import { relations } from "drizzle-orm";
-import { users } from "../server/models/auth";
+import { users } from "../models/auth";
 
 // =========================
 // DATABASE TABLES
